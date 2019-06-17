05f2c96d5fa17c01dd5daee21a59ad6d5f23ab44
Atte Sairanen <atte.sairanen@gmail.com>
2019-06-18 02:09:51 +0300

    Code refactoring, new exception class.

--- functions/add_workout/main.py
+++ functions/add_workout/main.py
@@ -1,61 +1,40 @@
-#import os
 import gpxpy
-#from google.cloud.datastore.helpers import GeoPoint # https://googleapis.github.io/google-cloud-python/latest/index.html
-#from google.cloud.firestore_v1beta1 import GeoPoint
 from firebase_admin.firestore import GeoPoint
 import firebase_admin
 from firebase_admin import firestore, auth
-from workout import Workout, Track, Segment
 from flask import abort, jsonify
+from flask import current_app as app
 from dataclasses import asdict
 
+from workout import Workout, Track, Segment
 
-def init_firebase():
-    app = firebase_admin.initialize_app()
 
-def add_workout(request):
-    """ Parses a 'multipart/form-data' upload request
+""" Parses a 'multipart/form-data' upload request that contains information for a workout
+    including a gpx file. Workout is saved to Firestore database.
     Args:
         request (flask.Request): The request object.
     Returns:
-        The response text, or any set of values that can be turned into a
-         Response object using `make_response`
-        <http://flask.pocoo.org/docs/1.0/api/#flask.Flask.make_response>.
-    """
-
-    if(request.method != 'POST'):
-        raise Exception('Invalid request method.')
-
-    # get auth token
-    auth_header = request.headers.get('Authorization')
-    if not auth_header:
-        raise Exception('Missing auth header')
-    
+        200 OK response if workout is added, otherwise 400 with JSON error response.
+"""
+def add_workout(request):
     try:
-        id_token = auth_header.split(" ")[1]
-    except IndexError:
-        raise Exception('Invalid auth header')
-    
-    try:
-        decoded_token = auth.verify_id_token(id_token) # not checking if revoked
-    except ValueError:
-        raise Exception('Invalid token')
+        return add_workout_to_firestore(request)
+    except InvalidUsage as e:
+        return e.to_response()  # return response without exception, no error logging
+    except Exception as e:
+        raise e # this will show up in google error reporting console
 
-    uid = decoded_token['uid']
+def add_workout_to_firestore(request):
+    # headers and form
+    check_request_method(request, ['POST'])
+    uid = get_uid(request)
+    check_file_size(request)
+    name = get_name(request)
+    gpx_file = get_file(request)
+    sport = get_sport(request)
+    description = get_description(request)
 
-    # TODO: check file size
-
-    name = request.form.get('name')
-    if (not name):
-        abort(make_error(400, 'nameRequired', 'Harjoituksen nimi vaaditaan.'))
-   
-    gpx_file = request.files.get('file')
-    if (not gpx_file):
-        abort(make_error(400, 'fileRequired', 'GPX-tiedosto vaaditaan.'))
-    
-    sport = request.form.get('sport')
-    description = request.form.get('description')
-
+    # parse gpx to workout
     try:
         # read and decode file because gpxpy cannot read file in binary mode
         gpx_string = gpx_file.read().decode('utf-8')
@@ -63,32 +42,73 @@ def add_workout(request):
         workout = Workout(gpx, name, uid, sport=sport, description=description)
     except Exception as e:
         print(e)
-        abort(make_error(400, 'fileInvalid', 'Virheellinen GPX-tiedosto.')) # TODO: add own expetion
+        raise InvalidUsage('Virheellinen GPX-tiedosto.', error_code='fileInvalid' )
 
-    if (not save_workout(workout)):
-        abort(make_error(400, 'saveFailed', 'Harjoituksen tallentaminen epäonnistui.'))
+    # save to firestore
+    try:
+        save_workout_to_firestore(workout)
+    except Exception as e:
+        print(e)
+        raise InvalidUsage('Harjoituksen tallentaminen epäonnistui.', error_code='saveFailed')
     
-    #TODO: return 201 Created (and added object?) 
+    #TODO: return 201 Created (and workout object?) 
 
-    return jsonify({'name': name, 'filename':gpx_file.filename})
+    return jsonify({'name': name, 'filename': gpx_file.filename})
+
+def init_firebase():
+    app = firebase_admin.initialize_app()
+
+def check_request_method(request, allowed_methods):
+    if(request.method not in allowed_methods):
+        raise InvalidUsage('Invalid request method.')
+    return request.method
+
+def check_file_size(request):
+    pass #TODO
+
+# Authtenticates request using token in the request header.
+def get_uid(request):
+    auth_header = request.headers.get('Authorization')
+    if not auth_header:
+        raise InvalidUsage('Missing auth header')
+    try:
+        id_token = auth_header.split(" ")[1]
+    except IndexError:
+        raise InvalidUsage('Invalid auth header')
+    
+    try:
+        decoded_token = auth.verify_id_token(id_token) # not checking if revoked
+    except ValueError:
+        raise InvalidUsage('Invalid token')
+
+    return decoded_token['uid']
+
+def get_name(request):
+    name = request.form.get('name')
+    if (not name):
+        raise InvalidUsage('Harjoituksen nimi vaaditaan.', error_code='nameRequired')
+    return name
+
+def get_file(request):
+    gpx_file = request.files.get('file')
+    if (not gpx_file):
+        raise InvalidUsage('GPX-tiedosto vaaditaan.', error_code='fileRequired')
+    return gpx_file
+
+def get_sport(request):
+    return request.form.get('sport')
+
+def get_description(request):
+    return request.form.get('description')
 
 
-def make_error(status_code, error_code, message):
-    response = jsonify({
-        'code': error_code,
-        'message': message
-    })
-    response.status_code = status_code
-    return response
-
-def save_workout(workout):
+def save_workout_to_firestore(workout):
     # convert workout to dict
     workout_dict = asdict(workout)
     # move track dicts to seprate list, they will be saved in a subcollection
     tracks = workout_dict.pop('tracks')
     if(not isinstance(tracks, list)):
-        print('Workout does not contain tracks list. Should have atleast an empty list.')
-        return False
+        raise Exception('Workout does not contain tracks list. Should have atleast an empty list.')
     
     # get firestore client, api docs here https://googleapis.github.io/google-cloud-python/latest/firestore/client.html
     db = firestore.client()
@@ -101,10 +121,38 @@ def save_workout(workout):
         track_ref = track_subcollection_ref.document()
         batch.set(track_ref, track)
     
-    batch.commit() #TODO: error handeling?
+    batch.commit()
     return True
 
 
+"""
+Custom exception class that can contain detailed error information that can be return as response.
+Error object can be turned into Response with to_response.
+"""
+class InvalidUsage(Exception):
+
+    def __init__(self, message, error_code=None, status_code=400, payload=None):
+        Exception.__init__(self)
+        self.message = message
+        self.status_code = status_code
+        self.error_code = error_code
+        self.payload = payload
+
+    def to_dict(self):
+        error = dict(self.payload or ())
+        error['message'] = self.message
+        error['code'] = self.error_code
+        return error
+
+    def to_response(self):
+        response = jsonify(self.to_dict()) # creates a Response with the JSON representation
+        response.status_code = self.status_code
+        return response
+    
 
 init_firebase()
 
+# Google cloud function does not give access to flask app instance, this does not work with google cloud functions:
+# @app.errorhandler(InvalidUsage)
+# def handle_invalid_usage(error):
+#     ...
